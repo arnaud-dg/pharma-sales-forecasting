48bca4608835ffc4bbabac5522544a86af923011
arnaud_dg <ad_learning@yahoo.com>
2023-10-31 15:19:23 +0000

    ok

--- 💲📈_Homepage.py
+++ 💲📈_Homepage.py
@@ -11,6 +11,19 @@ st.set_page_config(layout="wide")
 # Initializing du client S3
 s3 = boto3.client('s3')
 
+# Importing functions
+def fetch_data(SQL_query):
+    # Connection to snowflake and cursor creation
+    conn = snowflake.connector.connect(**st.secrets["snowflake"])
+    cur = conn.cursor()
+    cur.execute(SQL_query)
+    # Loading Data into a DataFrame
+    df = pd.DataFrame.from_records(iter(cur), columns=[x[0] for x in cur.description])
+    # Close the connection
+    cur.close()
+    conn.close()
+    return df
+
 def load_data_from_s3(bucket_name, file_key):
     """Get a .csv file from a S3 bucket and transform it as a dataframe"""
     response = s3.get_object(Bucket=bucket_name, Key=file_key)
@@ -28,16 +41,17 @@ product_list = list(df_product['ATC_Class'].unique())
 product_list.sort()
 product_list = product_list[:5]
 
-st.write(len(product_list))
-
 # Interface Streamlit
 st.title("🏥 French Pharmaceutical Sales Forecasting")
-
-st.dataframe(df_product)
 
 st.sidebar.write("""This web application, made with Streamlit, is a personal project I undertook to practice with Time-series Forecasting. 
                  The technical stack used implies AWS, Snowflake, SQL, and Python. 
                  The aim of this application is to provide a trend analysis and trend prediction of the drug consumption in France.""")
 # data_to_forecast = st.sidebar.radio("What kind of data do you wish to forecast",["***A drug family***", "***A product***", "***A reference (CIP code)***"])
-prediction_timeframe = st.sidebar.slider('How many months do you wish to predict?', 3, 12, 1)
+prediction_timeframe = st.sidebar.slider('How many months do you wish to predict?', min_value=3, value=6, max_value=12, step=1)
 selected_product = st.sidebar.selectbox('Which product would you like to forecast?', product_list)
+forecasting_method = st.sidebar.selectbox('Which product would you like to forecast?', product_list)
+
+# Get the data from snowflake
+df_chart = fetch_data("SELECT * FROM ATC1 WHERE ATC_Class = {}".format(selected_product))
+st.dataframe(df_chart)
