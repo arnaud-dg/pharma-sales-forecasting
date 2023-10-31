612d7ec648a24d0fd622b044ac8cc86940b94e5e
arnaud_dg <ad_learning@yahoo.com>
2023-10-31 15:25:51 +0000

    OK

--- 💲📈_Homepage.py
+++ 💲📈_Homepage.py
@@ -4,6 +4,7 @@ import boto3
 from io import BytesIO
 import streamlit as st
 import altair as alt
+import snowflake.connector
 
 # Layout of the main page
 st.set_page_config(layout="wide")
@@ -53,7 +54,7 @@ selected_product = st.sidebar.selectbox('Which product would you like to forecas
 forecasting_method = st.sidebar.selectbox('Which forecasting method would you like to apply', ['ARIMA', 'Prophet', 'LSTM', 'Exponential Smoothing', 'Linear Regression'])
 
 # Get the data from snowflake
-query = 'SELECT * FROM ATC1 WHERE ATC_Class = ' + selected_product + '"'
+query = 'SELECT * FROM ATC1 WHERE ATC_Class = "' + selected_product + '"'
 st.write(query)
 df_chart = fetch_data(query)
 st.dataframe(df_chart)
